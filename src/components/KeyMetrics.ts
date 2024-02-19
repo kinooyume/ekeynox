6ad01d7ad4958e4f6c9f44bff6c2e4484465f33c
Martin Kinoo <martkinoo@gmail.com>
2024-02-19 17:36:13 +0100

    Feature: KeyMetrics: add key projection

--- src/components/KeyMetrics.ts
+++ src/components/KeyMetrics.ts
@@ -1,3 +1,5 @@
+import { TypingStatusKind, type TypingStatus } from "./TypingEngine";
+
 export enum PromptKeyStatus {
   unset = "unset",
   correct = "correct",
@@ -13,7 +15,7 @@ export enum KeyStatus {
   ignore,
 }
 
-const blankCharacters = [" ", "\n", "\r"];
+const blankCharacters = [" ", "Enter"];
 
 type KeyMetrics =
   | { kind: KeyStatus.match }
@@ -55,4 +57,68 @@ const getKeyMetrics = ({
   return [typed, metrics];
 };
 
-export default getKeyMetrics;
+export type KeyMetricsProjection = {
+  correct: number;
+  incorrect: number;
+  extra: number;
+  missed: number;
+  deletedCorrect: number;
+  deletedIncorrect: number;
+  total: number;
+  expected: Array<string>;
+};
+
+const createKeyMetricsProjection = (): KeyMetricsProjection => ({
+  correct: 0,
+  incorrect: 0,
+  extra: 0,
+  missed: 0,
+  deletedCorrect: 0,
+  deletedIncorrect: 0,
+  total: 0,
+  expected: [],
+});
+
+export type KeysProjection = Record<string, KeyMetricsProjection>;
+
+type KeysProjectionProps = { projection: KeysProjection; status: TypingStatus };
+const updateKeyProjection = ({
+  projection,
+  status,
+}: KeysProjectionProps): KeysProjection => {
+  if (status.kind === TypingStatusKind.unstart) {
+    return {};
+  } else if (status.kind !== TypingStatusKind.pending) {
+    return projection;
+  }
+  const [key, metrics] = status.keyMetrics;
+  if (projection[key] === undefined) {
+    projection[key] = createKeyMetricsProjection();
+  }
+  switch (metrics.kind) {
+    case KeyStatus.match:
+      projection[key].correct++;
+      break;
+    case KeyStatus.unmatch:
+      projection[key].incorrect++;
+      projection[key].expected.push(metrics.expected);
+      break;
+    case KeyStatus.extra:
+      projection[key].extra++;
+      break;
+    case KeyStatus.missed:
+      projection[key].missed++;
+      projection[key].expected.push(metrics.expected);
+      break;
+    case KeyStatus.deleted:
+      if (metrics.status === PromptKeyStatus.correct) {
+        projection[key].deletedCorrect++;
+      } else {
+        projection[key].deletedIncorrect++;
+      }
+      break;
+  }
+  return projection;
+};
+
+export { getKeyMetrics, updateKeyProjection, createKeyMetricsProjection };
