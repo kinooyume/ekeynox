8ac993055c100ecf8bbd91ea9ab4f0ba04503f5e
Martin Kinoo <martkinoo@gmail.com>
2024-02-19 09:55:41 +0100

    Lint: Astro Config: lint & add compress html

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from 'astro/config';
-import solidStyled from 'unplugin-solid-styled';
+import { defineConfig } from "astro/config";
+import solidStyled from "unplugin-solid-styled";
 
 import solidJs from "@astrojs/solid-js";
 
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()], 
-vite:  {
+  integrations: [solidJs()],
+  compressHTML: true,
+  vite: {
     plugins: [
       solidStyled.vite({
-        prefix: 'example',
+        prefix: "example",
         filter: {
-          include: 'src/**/*.tsx',
-          exclude: 'node_modules/**/*.{ts,js}',
+          include: "src/**/*.tsx",
+          exclude: "node_modules/**/*.{ts,js}",
         },
       }),
     ],
