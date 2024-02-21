6edff9cac0a3e60ed9dcd678ab9b82eb9d3dfd8f
Martin Kinoo <martkinoo@gmail.com>
2024-02-21 19:50:56 +0100

    Feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import solidStyled from "unplugin-solid-styled";
+import sitemap from "@astrojs/sitemap";
 
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  site: "https://ekeynox.xyz",
+  integrations: [solidJs(), sitemap()],
   compressHTML: true,
   vite: {
     plugins: [
