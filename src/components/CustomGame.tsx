b2d614fe196f2b31a24409c4edde840834626e65
Martin Kinoo <martkinoo@gmail.com>
2024-02-26 00:49:37 +0100

    style(custom): enlarge textarea

--- src/components/CustomGame.tsx
+++ src/components/CustomGame.tsx
@@ -1,5 +1,6 @@
 import { Show, createSignal } from "solid-js";
 import TypingGame from "./TypingGame";
+import { css } from "solid-styled";
 
 const CustomGame = () => {
   const [text, setText] = createSignal("");
@@ -7,10 +8,16 @@ const CustomGame = () => {
 
   let inputRef: HTMLTextAreaElement;
 
+  css`
+    textarea {
+      width: 100%;
+      height: 200px;
+    }
+  `;
   return (
     <Show when={!ready()} fallback={<TypingGame source={text()} />}>
       <div>
-        <textarea  ref={inputRef!}></textarea>
+        <textarea ref={inputRef!}></textarea>
         <button
           onClick={() => {
             setText(inputRef.value);
