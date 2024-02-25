ac3ebb37fd83a1656b5b34e78522f1923954dc06
Martin Kinoo <martkinoo@gmail.com>
2024-02-25 22:29:39 +0100

    refacto(metricsresume): separate data

--- src/components/TypingMetricsResume.tsx
+++ src/components/TypingMetricsResume.tsx
@@ -5,6 +5,7 @@ import type { KeysProjection } from "./KeyMetrics";
 import type { KeyboardLayout } from "./KeyboardLayout";
 import MetricsChart from "./MetricsChart";
 import type { StatProjection } from "./KeypressMetrics";
+import DataMetricsResume from "./DataMetricsResume";
 
 type TypingMetricsProps = {
   stat: StatProjection;
@@ -23,15 +24,6 @@ const TypingMetricsResume = (props: TypingMetricsProps) => {
       max-width: 1200px;
       margin: 0 auto;
     }
-    .data {
-      display: flex;
-      position: fixed;
-      right: 224px;
-      flex-direction: column;
-      align-items: flex-start;
-      max-width: 400px;
-      margin: 24px auto;
-    }
 
     .keyboard {
       margin-top: 64px;
@@ -43,84 +35,10 @@ const TypingMetricsResume = (props: TypingMetricsProps) => {
       max-width: 1200px;
       margin-right: 198px;
     }
-    .speeds {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-    }
-    .speed-data {
-      display: flex;
-      flex-direction: column;
-      align-items: flex-start;
-    }
-    .wpm-data {
-      font-size: 4em;
-      margin: 0;
-    }
-    .wpm-data span {
-      font-size: 1.5rem;
-      margin-left: 8px;
-      opacity: 0.6;
-    }
-    .raw-data span {
-      margin-left: 4px;
-    }
-
-    .title {
-      font-size: 1.8rem;
-    }
-    .accuracies-data {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-    }
-    .accu-data {
-      font-size: 3em;
-      margin: 0;
-    }
-    .accu-data span {
-      font-size: 1.5rem;
-      opacity: 0.6;
-    }
-    .accu-real {
-      font-size: 2rem;
-    }
-    .accu-real-data span {
-      font-size: 0.9rem;
-      opacity: 0.6;
-    }
   `;
   return (
     <div class="metrics">
-      <div class="data">
-        <div class="speeds">
-          <p class="title">Speed</p>
-          <div class="speed-data">
-            <p class="wpm-data">
-              {props.stat.speed.byWord[0].toFixed(2)}
-              <span>WPM</span>
-            </p>
-            <p class="raw-data">
-              {props.stat.speed.byKeypress[1].toFixed(2)}
-              <span>Raw</span>
-            </p>
-          </div>
-        </div>
-        <div class="accuracies">
-          <p class="title">Accuracy</p>
-          <div class="accuracies-data">
-            <p class="accu-data">
-              {Math.trunc(props.stat.accuracies[0])}
-              <span>%</span>
-            </p>
-            <p class="accu-real-data">
-              Real : {props.stat.accuracies[1].toFixed(2)}
-              <span>%</span>
-            </p>
-          </div>
-        </div>
-      </div>
-
+      <DataMetricsResume stat={props.stat} />
       <div class="details">
         <div class="chart">
           <MetricsChart metrics={props.metrics} />
