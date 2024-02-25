dca9cd2054b9fb7a313bbf0e2b4bfcdc04c90413
Martin Kinoo <martkinoo@gmail.com>
2024-02-25 19:50:13 +0100

    style(metricsresume): change a bit the sidebar

--- src/components/TypingMetricsResume.tsx
+++ src/components/TypingMetricsResume.tsx
@@ -28,7 +28,7 @@ const TypingMetricsResume = (props: TypingMetricsProps) => {
       position: fixed;
       right: 224px;
       flex-direction: column;
-      align-items: center;
+      align-items: flex-start;
       max-width: 400px;
       margin: 24px auto;
     }
@@ -46,11 +46,33 @@ const TypingMetricsResume = (props: TypingMetricsProps) => {
     .speeds {
       display: flex;
       flex-direction: column;
-      align-items: flex-end;
+      align-items: flex-start;
+    }
+    .speed-data {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
     }
     .wpm-data {
       font-size: 4em;
       margin: 0;
+    }
+    .wpm-data span {
+      font-size: 1.5rem;
+      margin-left: 8px;
+      opacity: 0.6;
+    }
+    .raw-data span {
+      margin-left: 4px;
+    }
+
+    .title {
+      font-size: 1.8rem;
+    }
+    .accuracies-data {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
     }
     .accu-data {
       font-size: 3em;
@@ -58,25 +80,44 @@ const TypingMetricsResume = (props: TypingMetricsProps) => {
     }
     .accu-data span {
       font-size: 1.5rem;
+      opacity: 0.6;
+    }
+    .accu-real {
+      font-size: 2rem;
+    }
+    .accu-real-data span {
+      font-size: 0.9rem;
+      opacity: 0.6;
     }
   `;
   return (
     <div class="metrics">
       <div class="data">
         <div class="speeds">
-          <p class="wpm-data">{props.stat.speed.byWord[0].toFixed(2)}</p>
-          <p>WPM</p>
-          <p class="speed">Raw: {props.stat.speed.byKeypress[1].toFixed(2)}</p>
+          <p class="title">Speed</p>
+          <div class="speed-data">
+            <p class="wpm-data">
+              {props.stat.speed.byWord[0].toFixed(2)}
+              <span>WPM</span>
+            </p>
+            <p class="raw-data">
+              {props.stat.speed.byKeypress[1].toFixed(2)}
+              <span>Raw</span>
+            </p>
+          </div>
         </div>
         <div class="accuracies">
-          <p class="accu-data">
-            {Math.trunc(props.stat.accuracies[0])}
-            <span>%</span>
-          </p>
-          <p>Accuracy</p>
-          <p class="card accuracy">
-            Real : {props.stat.accuracies[1].toFixed(2)}%
-          </p>
+          <p class="title">Accuracy</p>
+          <div class="accuracies-data">
+            <p class="accu-data">
+              {Math.trunc(props.stat.accuracies[0])}
+              <span>%</span>
+            </p>
+            <p class="accu-real-data">
+              Real : {props.stat.accuracies[1].toFixed(2)}
+              <span>%</span>
+            </p>
+          </div>
         </div>
       </div>
 
