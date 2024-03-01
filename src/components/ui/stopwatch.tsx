6211537645766abe7fe97990e8af7eba969bd681
Martin Kinoo <martkinoo@gmail.com>
2024-03-01 19:48:24 +0100

    svg: add severals icons

---
+++ src/components/ui/stopwatch.tsx
@@ -0,0 +1,6 @@
+
+const Stopwatch = () => <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+	<path fill="var(--text-color)" d="M12 5c-4.411 0-8 3.589-8 8s3.589 8 8 8s8-3.589 8-8s-3.589-8-8-8m1 8h-2V8h2zM9 2h6v2H9zm9.707 2.293l2 2l-1.414 1.414l-2-2z" />
+</svg>
+
+export default Stopwatch;
