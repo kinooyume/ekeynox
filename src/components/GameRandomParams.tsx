f80b6da76e5a7cd078ccdff3b411447095c0d761
Martin Kinoo <martkinoo@gmail.com>
2024-03-01 19:48:44 +0100

    params: implement icons

--- src/components/GameRandomParams.tsx
+++ src/components/GameRandomParams.tsx
@@ -10,6 +10,9 @@ import {
 } from "./App";
 import RadioGroup from "./RadioGroup";
 import { Match, Show, Switch } from "solid-js";
+import Quote from "./ui/quote";
+import Text from "./ui/text";
+import Customizer from "./ui/customizer";
 
 type GameRandomParamsProps = {
   start: (content?: string) => void;
@@ -46,9 +49,9 @@ const GameRandomParams = (props: GameRandomParamsProps) => {
       <RadioGroup
         name="wordsCategory"
         values={[
-          { label: props.t("words"), value: WordsCategory.words1k },
-          { label: props.t("quotes"), value: WordsCategory.quotes },
-          { label: props.t("custom"), value: WordsCategory.custom },
+          { label: props.t("words"), value: WordsCategory.words1k, icon: <Text /> },
+          { label: props.t("quotes"), value: WordsCategory.quotes, icon: <Quote /> },
+          { label: props.t("custom"), value: WordsCategory.custom, icon: <Customizer /> },
         ]}
         checked={props.wordsCategory}
         setChecked={props.setWordsCategory}
