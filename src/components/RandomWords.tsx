f21210c377110384db6a8c2c3d61464bfb9d9847
Martin Kinoo <martkinoo@gmail.com>
2024-03-01 15:11:03 +0100

    feat(randomwords): better random with crypto

--- src/components/RandomWords.tsx
+++ src/components/RandomWords.tsx
@@ -1,10 +1,15 @@
-const randomWords = (data: Array<string>) => (nbr: number) : string => {
-  const words = [];
-  for (let i = 0; i < nbr; i++) {
-    const index = Math.floor(Math.random() * data.length);
-    words.push(data[index]);
-  }
-  return words.join(" ");
-};
+const randomWords =
+  (data: Array<string>) =>
+  (nbr: number): string => {
+    const words = [];
+    for (let i = 0; i < nbr; i++) {
+      const index = Math.floor(
+        (crypto.getRandomValues(new Uint32Array(1))[0] / 4294967296) *
+          data.length,
+      );
+      words.push(data[index]);
+    }
+    return words.join(" ");
+  };
 
 export default randomWords;
