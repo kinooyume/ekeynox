25e3d4c5ef7845c01f7fa729c84715ad7a0448f0
Martin Kinoo <martkinoo@gmail.com>
2024-03-02 14:28:12 +0100

    fix(content): add space between parseWords

--- src/components/Content.ts
+++ src/components/Content.ts
@@ -19,7 +19,7 @@ export type MetaWord = {
 export type Paragraph = Array<MetaWord>;
 export type Paragraphs = Array<Paragraph>;
 
-const Enter = () => ({
+const makeEnter = () => ({
   focus: false,
   status: WordStatus.unstart,
   isSeparator: true,
@@ -34,38 +34,71 @@ const Enter = () => ({
   ],
 });
 
-export type Parser = (source: string) => [Paragraphs, Set<string>];
+const makeSpace = () => ({
+  focus: false,
+  status: WordStatus.unstart,
+  isSeparator: true,
+  wasCorrect: false,
+  wpm: 0,
+  keys: [
+    {
+      key: " ",
+      status: PromptKeyStatus.unstart,
+      focus: PromptKeyFocus.unset,
+    },
+  ],
+});
+
+export type ContentData = [Paragraphs, Set<string>];
+const parseWord =
+  (keySet: Set<string>) =>
+  ({ word }: { word: string }) => ({
+    focus: false,
+    status: WordStatus.unstart,
+    wasCorrect: false,
+    wpm: 0,
+    isSeparator: word.trim() === "",
+    keys: word.split("").map((key) => {
+      keySet.add(key);
+      return {
+        key,
+        status: PromptKeyStatus.unstart,
+        focus: PromptKeyFocus.unset,
+      };
+    }),
+  });
+
+export type Parser = (source: string) => ContentData;
 export const parse: Parser = (source) => {
   const keySet = new Set<string>();
+  const wordParser = parseWord(keySet);
   const paragraphs = source
     .split("\n")
     .map((line) => {
       return line
         .split(/(\s+)/)
-        .map((word) => ({
-          focus: false,
-          status: WordStatus.unstart,
-          wasCorrect: false,
-          wpm: 0,
-          isSeparator: word.trim() === "",
-          keys: word.split("").map((key) => {
-            keySet.add(key);
-            return {
-              key,
-              status: PromptKeyStatus.unstart,
-              focus: PromptKeyFocus.unset,
-            };
-          }),
-        }))
+        .map((word) => wordParser({ word }))
         .filter((word) => word.keys.length > 0);
     })
     .filter((paragraph) => paragraph.length > 0);
+  // TODO: use a flatMap
   if (paragraphs.length > 1) {
     for (let i = 0; i < paragraphs.length - 1; i++) {
-      paragraphs[i].push(Enter());
+      paragraphs[i].push(makeEnter());
     }
   }
   return [paragraphs, keySet];
+};
+
+const parseWords = (source: Array<string>): ContentData => {
+  const keySet = new Set<string>();
+  const wordParser = parseWord(keySet);
+  const words = source.flatMap((word, index) =>
+    index < source.length - 1
+      ? [wordParser({ word }), makeSpace()]
+      : wordParser({ word }),
+  );
+  return [[words], keySet];
 };
 
 const deepClone = (paragraphs: Paragraphs) =>
@@ -78,5 +111,6 @@ const deepClone = (paragraphs: Paragraphs) =>
 
 export default {
   parse,
+  parseWords,
   deepClone,
 };
