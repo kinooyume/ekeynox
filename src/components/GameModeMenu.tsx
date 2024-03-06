c2c83c266d4bb7516b8b682670f759e996dfbd93
Martin Kinoo <martkinoo@gmail.com>
2024-03-06 16:39:33 +0100

    feat(modeselection): add picto/icon, and show text on hover

--- src/components/GameModeMenu.tsx
+++ src/components/GameModeMenu.tsx
@@ -13,7 +13,7 @@ import {
 import GameTimerParams from "./GameTimerParams";
 import GameModeSelection from "./GameModeSelection";
 import Bunny from "./ui/bunny";
-import { Transition } from "solid-transition-group"
+import { Transition } from "solid-transition-group";
 // Gsap animation
 // https://codepen.io/dev_loop/pen/MWKbJmO
 // Store like this
@@ -73,6 +73,7 @@ const GameModeMenu = (props: GameModeMenuProps) => {
       grid-template-rows: 1fr;
       grid-column-gap: 0px;
       grid-row-gap: 0px;
+      overflow: hidden;
     }
     .title {
       position: absolute;
@@ -112,7 +113,7 @@ const GameModeMenu = (props: GameModeMenuProps) => {
     .selection {
       position: absolute;
       bottom: 0;
-      right: 0;
+      right: 32px;
     }
     h2 {
       font-size: 4rem;
@@ -131,114 +132,99 @@ const GameModeMenu = (props: GameModeMenuProps) => {
     }
   `;
   return (
-    <Transition
-      onEnter={(el , done) => {
-        const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
-          duration: 600,
-        });
-        a.finished.then(done);
-      }}
-      onExit={(el, done) => {
-        const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
-          duration: 600,
-        });
-        a.finished.then(done);
-      }}
-    >
-      <div class="menu">
-        <div class="main-view">
-          <div class="title">
-            <div class="title-content">
-              <span>{props.t("chooseYour")}</span>
-              <h1>{props.t("playingMode")}</h1>
-            </div>
-          </div>
-          <div class="cliped">
-            <div class="illustration">
-              <div class="illustration-container">
-                <Switch>
-                  <Match
-                    when={props.gameOptions.lastGameMode === GameMode.monkey}
-                  >
-                    <Bunny />
-                  </Match>
-                  <Match
-                    when={props.gameOptions.lastGameMode === GameMode.rabbit}
-                  >
-                    <Bunny />
-                  </Match>
-                </Switch>
-              </div>
-            </div>
-            <Switch>
-              <Match when={props.gameOptions.lastGameMode === GameMode.monkey}>
-                <div class="option">
-                  <div class="text">
-                    <h2>{props.t("gameMode.monkey.title")}</h2>
-                    <h3>{props.t("gameMode.monkey.subtitle")}</h3>
-                    <p class="description">
-                      {props.t("gameMode.monkey.hugeDescription")}
-                    </p>
-                  </div>
-                  <GameRandomParams
-                    start={setMonkey}
-                    words={props.gameOptions.wordNumber}
-                    language={props.gameOptions.language}
-                    wordsCategory={props.gameOptions.wordsCategory}
-                    setWords={(words) =>
-                      props.setGameOptions("wordNumber", words)
-                    }
-                    setLanguage={(language) =>
-                      props.setGameOptions("language", language)
-                    }
-                    setWordsCategory={(wordsCategory) =>
-                      props.setGameOptions("wordsCategory", wordsCategory)
-                    }
-                    t={props.t}
-                  />
-                </div>
-              </Match>
-              <Match when={props.gameOptions.lastGameMode === GameMode.rabbit}>
-                <div class="option">
-                  <div class="text">
-                    <h2>{props.t("gameMode.rabbit.title")}</h2>
-                    <h3>{props.t("gameMode.rabbit.subtitle")}</h3>
-                    <p class="description">
-                      {props.t("gameMode.rabbit.hugeDescription")}
-                    </p>
-                  </div>
-                  <GameTimerParams
-                    start={setRabbit}
-                    time={props.gameOptions.time}
-                    language={props.gameOptions.language}
-                    wordsCategory={props.gameOptions.wordsCategory}
-                    setTime={(time) => props.setGameOptions("time", time)}
-                    setLanguage={(language) =>
-                      props.setGameOptions("language", language)
-                    }
-                    setWordsCategory={(wordsCategory) =>
-                      props.setGameOptions("wordsCategory", wordsCategory)
-                    }
-                    t={props.t}
-                  />
-                </div>
-              </Match>
-            </Switch>
-          </div>
-          <div class="selection">
-            <GameModeSelection
-              t={props.t}
-              modes={{
-                rabbit: props.t("gameMode.rabbit"),
-                monkey: props.t("gameMode.monkey"),
-              }}
-              selected={props.gameOptions.lastGameMode}
-              setSelected={(mode) => props.setGameOptions("lastGameMode", mode)}
-            />
+    <div class="menu">
+      <div class="main-view">
+        <div class="title">
+          <div class="title-content">
+            <span>{props.t("chooseYour")}</span>
+            <h1>{props.t("playingMode")}</h1>
           </div>
         </div>
+        <div class="cliped">
+          <div class="illustration">
+            <div class="illustration-container">
+              <Switch>
+                <Match
+                  when={props.gameOptions.lastGameMode === GameMode.monkey}
+                >
+                  <Bunny />
+                </Match>
+                <Match
+                  when={props.gameOptions.lastGameMode === GameMode.rabbit}
+                >
+                  <Bunny />
+                </Match>
+              </Switch>
+            </div>
+          </div>
+          <Switch>
+            <Match when={props.gameOptions.lastGameMode === GameMode.monkey}>
+              <div class="option">
+                <div class="text">
+                  <h2>{props.t("gameMode.monkey.title")}</h2>
+                  <h3>{props.t("gameMode.monkey.subtitle")}</h3>
+                  <p class="description">
+                    {props.t("gameMode.monkey.hugeDescription")}
+                  </p>
+                </div>
+                <GameRandomParams
+                  start={setMonkey}
+                  words={props.gameOptions.wordNumber}
+                  language={props.gameOptions.language}
+                  wordsCategory={props.gameOptions.wordsCategory}
+                  setWords={(words) =>
+                    props.setGameOptions("wordNumber", words)
+                  }
+                  setLanguage={(language) =>
+                    props.setGameOptions("language", language)
+                  }
+                  setWordsCategory={(wordsCategory) =>
+                    props.setGameOptions("wordsCategory", wordsCategory)
+                  }
+                  t={props.t}
+                />
+              </div>
+            </Match>
+            <Match when={props.gameOptions.lastGameMode === GameMode.rabbit}>
+              <div class="option">
+                <div class="text">
+                  <h2>{props.t("gameMode.rabbit.title")}</h2>
+                  <h3>{props.t("gameMode.rabbit.subtitle")}</h3>
+                  <p class="description">
+                    {props.t("gameMode.rabbit.hugeDescription")}
+                  </p>
+                </div>
+                <GameTimerParams
+                  start={setRabbit}
+                  time={props.gameOptions.time}
+                  language={props.gameOptions.language}
+                  wordsCategory={props.gameOptions.wordsCategory}
+                  setTime={(time) => props.setGameOptions("time", time)}
+                  setLanguage={(language) =>
+                    props.setGameOptions("language", language)
+                  }
+                  setWordsCategory={(wordsCategory) =>
+                    props.setGameOptions("wordsCategory", wordsCategory)
+                  }
+                  t={props.t}
+                />
+              </div>
+            </Match>
+          </Switch>
+        </div>
+        <div class="selection">
+          <GameModeSelection
+            t={props.t}
+            modes={{
+              rabbit: props.t("gameMode.rabbit"),
+              monkey: props.t("gameMode.monkey"),
+            }}
+            selected={props.gameOptions.lastGameMode}
+            setSelected={(mode) => props.setGameOptions("lastGameMode", mode)}
+          />
+        </div>
       </div>
-    </Transition>
+    </div>
   );
 };
 
