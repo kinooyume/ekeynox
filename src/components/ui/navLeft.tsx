2aeb7a8957d79d60ed7f1383651c271b04b1edb0
Martin Kinoo <martkinoo@gmail.com>
2024-03-10 12:30:00 +0100

    style: nav, fill with surface alt

--- src/components/ui/navLeft.tsx
+++ src/components/ui/navLeft.tsx
@@ -2,8 +2,8 @@ const NavLeft = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="205.004" height="63.668">
     <path
       d="M.5 63.014V49.098c26.241-.218 40.318-.659 44.105-1.949 5.302-1.807 10.889-5.508 26.641-21.871 6.737-6.998 13.91-13.726 18.955-18.064 3.217-2.766 9.388-5.784 13.6-6.051 9.698-.613 39.797-.699 100.703-.65v62.656L.5 63.014z"
-      fill="var(--key-color)"
-      stroke="var(--key-color)"
+      fill="var(--color-surface-alt)"
+      stroke="var(--color-surface-alt)"
     />
   </svg>
 );
