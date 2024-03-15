1ca5ec26cfc156632fa4be3e68e0f9f143ca0079
Martin Kinoo <martkinoo@gmail.com>
2024-03-15 12:51:49 +0100

    feat: List: add circular list

--- src/components/List.ts
+++ src/components/List.ts
@@ -3,10 +3,36 @@ export type LinkedList<T> = {
   next: LinkedList<T> | null;
 } | null;
 
-const make = <T>(list: LinkedList<T> | null, value: T): LinkedList<T> => {
-  return { value, next: list };
+const make = <T>(next: LinkedList<T> | null, value: T): LinkedList<T> => ({
+  value,
+  next,
+});
+
+export type CircularList<T> = {
+  value: T;
+  prev: CircularList<T>;
+  next: CircularList<T>;
 };
+
+const makeCircular = <T>(
+  value: T,
+  prev: CircularList<T>,
+  next: CircularList<T>,
+): CircularList<T> => ({ value, prev, next });
+
+const makeCircularNext =
+  <T>(value: T, prev: CircularList<T>) =>
+  (next: CircularList<T>) =>
+    makeCircular(value, prev, next);
+
+const makeCircularPrev =
+  <T>(value: T, next: CircularList<T>) =>
+  (prev: CircularList<T>) =>
+    makeCircular(value, prev, next);
 
 export default {
   make,
+  makeCircular,
+  makeCircularNext,
+  makeCircularPrev,
 };
