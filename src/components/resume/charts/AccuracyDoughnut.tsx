8ae9b63d6d6aee25a45842f75fda2249c4d723a5
Martin Kinoo <martkinoo@gmail.com>
2024-03-19 17:32:21 +0100

    feat(resume): add accuracy doughnut

---
+++ src/components/resume/charts/AccuracyDoughnut.tsx
@@ -0,0 +1,84 @@
+import { onMount, type JSXElement } from "solid-js";
+import {
+  Chart,
+  CategoryScale,
+  PointElement,
+  type ChartData,
+  Title,
+  Tooltip,
+  Legend,
+  Colors,
+  DoughnutController,
+} from "chart.js";
+
+import { DefaultChart } from "solid-chartjs";
+import { css } from "solid-styled";
+import type { StatProjection } from "../../metrics/KeypressMetrics";
+
+type MyChartProps = {
+  stats: StatProjection;
+  children: JSXElement;
+};
+const AccuracyDoughnut = (props: MyChartProps) => {
+  /**
+   * You must register optional elements before using the chart,
+   * otherwise you will have the most primitive UI
+   */
+  onMount(() => {
+    Chart.register(
+      CategoryScale,
+      DoughnutController,
+      PointElement,
+      Title,
+      Tooltip,
+      Legend,
+      Colors,
+    );
+  });
+
+  const correct = props.stats.accuracies[1];
+  const corrected = props.stats.accuracies[0] - props.stats.accuracies[1];
+  const incorrect = 100 - props.stats.accuracies[0];
+  console.log(correct, corrected, incorrect);
+  const data = {
+    datasets: [
+      {
+        data: [correct, corrected, incorrect],
+        backgroundColor: ["#107b65", "#b16f4c", "#a83f3f"],
+      },
+    ],
+  } as ChartData;
+
+  css`
+    .chart {
+      position: relative;
+      width: 260px;
+      height: 260px;
+    }
+    .center {
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+  `;
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    cutout: "80%",
+  };
+
+  return (
+    <div class="chart">
+      <DefaultChart type="doughnut" data={data} options={options} />
+      <div class="center">{props.children}</div>
+    </div>
+  );
+};
+
+export default AccuracyDoughnut;
