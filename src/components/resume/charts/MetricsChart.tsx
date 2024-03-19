a47bb36593cda3417287502ee9586a8c3f2cd134
Martin Kinoo <martkinoo@gmail.com>
2024-03-19 15:16:44 +0100

    fix(metricschart): proper display error scatter

--- src/components/resume/charts/MetricsChart.tsx
+++ src/components/resume/charts/MetricsChart.tsx
@@ -15,7 +15,7 @@ import {
 } from "chart.js";
 
 import { DefaultChart } from "solid-chartjs";
-import type { TypingMetrics } from "./TypingMetrics";
+import type { TypingMetrics } from "../../metrics/TypingMetrics";
 import { css } from "solid-styled";
 
 type MyChartProps = {
@@ -51,28 +51,29 @@ const MyChart = (props: MyChartProps) => {
   `;
   createComputed(() => {
     let labels = [] as string[];
-    let wpm = [] as number[];
-    let raw = [] as number[];
+    let wpm = [] as { x: number; y: number }[];
+    let raw = [] as { x: number; y: number }[];
     let incorrect = [] as { x: number; y: number }[];
 
     let log = props.metrics.logs;
+    let prevElapsed = -1;
     while (log) {
-      const elapsed = Math.round(
-        (log.value.core.duration) / 1000,
-      );
-
-      labels.push(elapsed.toString());
-      wpm.push(log.value.stats.speed.byWord[0]);
-      raw.push(log.value.stats.speed.byKeypress[1]);
-      let secProj = log.value.meta.sectionProjection;
-      let wrong = secProj.incorrect + secProj.missed + secProj.extra;
-      if (wrong > 0) {
-        incorrect.push({ y: elapsed, x: wrong });
+      const elapsed = Math.round(log.value.core.duration / 1000);
+      console.log(elapsed);
+      if (prevElapsed !== elapsed) {
+        labels.push(elapsed.toString());
+        wpm.push({ x: elapsed, y: log.value.stats.speed.byWord[0] });
+        raw.push({ x: elapsed, y: log.value.stats.speed.byKeypress[1] });
+        let secProj = log.value.meta.sectionProjection;
+        let wrong = secProj.incorrect + secProj.missed + secProj.extra;
+        if (wrong > 0) {
+          incorrect.push({ x: elapsed, y: wrong });
+        }
       }
+      prevElapsed = elapsed;
       log = log.next;
     }
     setData({
-      labels: labels.reverse(),
       datasets: [
         {
           type: "line",
@@ -80,7 +81,7 @@ const MyChart = (props: MyChartProps) => {
           order: 2,
           yAxisID: "wpm",
           borderColor: "#744307",
-          data: wpm.reverse(),
+          data: wpm,
         },
         {
           type: "line",
@@ -88,13 +89,14 @@ const MyChart = (props: MyChartProps) => {
           order: 3,
           yAxisID: "raw",
           borderColor: "#0f2c4e",
-          data: raw.reverse(),
+          data: raw,
         },
         {
           type: "scatter",
           label: "Errors",
           yAxisID: "error",
           pointStyle: "crossRot",
+          showLine: false,
           order: 1,
           borderColor: "#ff0000",
           data: incorrect,
@@ -109,11 +111,12 @@ const MyChart = (props: MyChartProps) => {
     scales: {
       x: {
         axis: "x",
-        ticks: {
-          autoSkip: true,
-          autoSkipPadding: 20,
-        },
+        type: "linear",
         display: true,
+        min: 1,
+        ticks: {
+          stepSize: 1,
+        },
         title: {
           display: true,
           text: "Seconds",
@@ -146,6 +149,7 @@ const MyChart = (props: MyChartProps) => {
         beginAtZero: true,
         min: 0,
         ticks: {
+          precision: 0,
           autoSkip: true,
           autoSkipPadding: 20,
         },
@@ -176,7 +180,7 @@ const MyChart = (props: MyChartProps) => {
 
   return (
     <div class="chart">
-      <DefaultChart type="line" data={data()} options={options} />
+      <DefaultChart data={data()} options={options} />
     </div>
   );
 };
