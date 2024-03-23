0d9624b53f76dbc6a00d83990f2eac30f4764559
Martin Kinoo <martkinoo@gmail.com>
2024-03-23 14:47:07 +0100

    feat(doughnut): overlaping segment

--- src/components/resume/charts/AccuracyDoughnut.tsx
+++ src/components/resume/charts/AccuracyDoughnut.tsx
@@ -3,6 +3,7 @@ import {
   CategoryScale,
   PointElement,
   type ChartData,
+  type ChartOptions,
   Title,
   Tooltip,
   Legend,
@@ -22,7 +23,6 @@ type MyChartProps = {
 };
 
 const AccuracyDoughnut = (props: MyChartProps) => {
-
   const correct = props.stats.accuracies[1];
   const corrected = props.stats.accuracies[0] - props.stats.accuracies[1];
   const incorrect = 100 - props.stats.accuracies[0];
@@ -35,11 +35,63 @@ const AccuracyDoughnut = (props: MyChartProps) => {
     ],
   } as ChartData;
 
+  type OverlapSegment = {
+    radius: number;
+    color: string;
+    coordinates: { x: number; y: number }[];
+  };
+
+  const makeOverlapSegment = (value: any, color: string) : OverlapSegment => {
+    const { innerRadius, outerRadius, endAngle } = value;
+    const radius = (outerRadius - innerRadius) / 2;
+    const coordinates = [];
+
+    for (let i = -0.01; i < 0.01; i += 0.01) {
+      const xCoor = (innerRadius + radius) * Math.cos(endAngle + Math.PI + i);
+      const yCoor = (innerRadius + radius) * Math.sin(endAngle + i);
+      coordinates.push({ x: xCoor, y: yCoor });
+    }
+    return { radius, color: "black", coordinates };
+  };
+
+  const overlappingSegments = {
+    id: "overlappingSegments",
+    afterDatasetsDraw(chart: Chart) {
+      const { ctx } = chart;
+      const x = chart.getDatasetMeta(0).data[0].x;
+      const y = chart.getDatasetMeta(0).data[0].y;
+      const angle = Math.PI / 180;
+
+      const overlaps = chart
+        .getDatasetMeta(0)
+        .data.filter((_, index) => chart.getDataVisibility(index))
+        .map((value, index) => {
+          return makeOverlapSegment(
+            value,
+            data!.datasets[0]!.backgroundColor![index]!,
+          );
+        });
+      overlaps.reverse().forEach(({ radius, color, coordinates }) => {
+        ctx.save();
+        ctx.translate(x, y);
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        coordinates.forEach(({ x, y }) => {
+          ctx.arc(-x, y, radius -1, 0, angle * 360, false);
+        });
+        ctx.fill();
+
+        ctx.restore();
+      });
+    },
+  };
+
   const options = {
     responsive: true,
     maintainAspectRatio: false,
     cutout: "80%",
-  };
+    plugins: [overlappingSegments],
+  } as ChartOptions;
 
   css`
     .chart {
@@ -61,16 +113,22 @@ const AccuracyDoughnut = (props: MyChartProps) => {
   `;
   return (
     <div class="chart">
-      <DefaultChart type="doughnut" data={data} plugins={[
-      CategoryScale,
-      DoughnutController,
-      PointElement,
-      Title,
-      Tooltip,
-      ArcElement,
-      Legend,
-      Colors,
-      ]} options={options} />
+      <DefaultChart
+        type="doughnut"
+        data={data}
+        plugins={[
+          CategoryScale,
+          DoughnutController,
+          PointElement,
+          Title,
+          Tooltip,
+          ArcElement,
+          Legend,
+          Colors,
+          overlappingSegments,
+        ]}
+        options={options}
+      />
       <div class="center">{props.children}</div>
     </div>
   );
