4811ec21ef4a4604bbcdab837044b52523fe22b9
Martin Kinoo <martkinoo@gmail.com>
2024-03-25 12:59:24 +0100

    feat(nav): more icons !

--- src/components/svgs/play.tsx
+++ src/components/svgs/play.tsx
@@ -1,32 +1,37 @@
+import { createEffect, createSignal } from "solid-js";
 import { css } from "solid-styled";
 
-const Play = ({ pause: boolean }) => {
-  let pause;
-  let pauses;
+type PlayProps = {
+  paused: boolean;
+};
+const Play = (props: PlayProps) => {
+  const getPaths = () =>
+    props.paused
+      ? ["M13 15L8 18L8 6L13 9L13 15", "M13 9L18 12L18 12L13 15L13 9"]
+      : ["M9 6L9 18L7 18L7 6z", "M15 6L17 6L17 18L15 18L15 6"];
 
+  const [paths, setPaths] = createSignal(getPaths());
+
+  createEffect(() => {
+    setPaths(getPaths());
+  });
   css`
     svg {
       cursor: pointer;
     }
     path {
-      fill: grey;
-      transition: ease-in-out 0.2s;
+      transition: 0.2s;
     }
-    svg:hover path {
-      transition: ease-in-out 0.2s;
+
+    svg:hover .paused path {
       fill: black;
     }
 
-    @keyframes play {
-      0% {
-        transform: translateX(0);
-      }
-      50% {
-        transform: translateX(4px);
-      }
-      100% {
-        transform: translateX(0);
-      }
+    .center {
+      opacity: 1;
+    }
+    .paused .center {
+      opacity: 0;
     }
   `;
   return (
@@ -36,30 +41,16 @@ const Play = ({ pause: boolean }) => {
       height="1.5em"
       viewBox="0 0 24 24"
     >
-      <g stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-        <path d="M9 6L9 18L7 18L7 6z">
-          <animate
-            ref={pause}
-            fill="freeze"
-            attributeName="d"
-            dur="0.4s"
-            values="M9 18L7 18L7 6L9 6L9 18;M13 15L8 18L8 6L13 9L13 15"
-          />
-          <set attributeName="opacity" begin="0.4s" to="0" />
-        </path>
-        <path d="M15 6L17 6L17 18L15 18L15 6">
-          <animate
-            ref={pauses}
-            fill="freeze"
-            attributeName="d"
-            dur="0.4s"
-            values="M15 6L17 6L17 18L15 18L15 6;M13 9L18 12L18 12L13 15L13 9"
-          />
-          <set attributeName="opacity" begin="0.4s" to="0" />
-        </path>
-        <path d="M8 6L18 12L8 18z" opacity="0">
-          <set attributeName="opacity" begin="0.4s" to="1" />
-        </path>
+      <g
+        classList={{ paused: !props.paused }}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        fill="grey"
+        stroke-width="2"
+      >
+        <path class="left" d={paths()[0]} />
+        <path class="right" d={paths()[1]} />
+        <path class="center" d="M8 6L18 12L8 18z" />
       </g>
     </svg>
   );
