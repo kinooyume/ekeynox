3bb3a1e54c666a9129d5c0481a58a28b4a819bea
Martin Kinoo <martkinoo@gmail.com>
2024-04-17 20:31:11 +0200

    flavor(cursornav): add CursorNavHook, impl for user

---
+++ src/components/cursor/CursorNavHooks.ts
@@ -0,0 +1,17 @@
+import { type Cursor } from "./Cursor";
+
+export type NodeHooks = {
+  enter: (cursor: Cursor) => void;
+  leave: (cursor: Cursor) => void;
+};
+
+export type NavHooks = {
+  prev: NodeHooks;
+  next: NodeHooks;
+};
+
+export type CursorNavHooks = {
+  paragraph: NavHooks;
+  word: NavHooks;
+  key: NavHooks;
+};
