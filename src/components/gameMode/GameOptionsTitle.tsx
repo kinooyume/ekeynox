9ca4db77908c5eb34de871df214717df9a83b360
Martin Kinoo <martkinoo@gmail.com>
2024-05-12 02:04:05 +0200

    flavor(head): add picto head smile

--- src/components/gameMode/GameOptionsTitle.tsx
+++ src/components/gameMode/GameOptionsTitle.tsx
@@ -12,17 +12,29 @@ type GameOptionsTitleProps = {
 const GameOptionsTitle = (props: GameOptionsTitleProps) => {
   css`
     .picto {
-      height: 200px;
-      width: 200px;
+      width: 180px;
     }
     .game-title {
       display: flex;
+      gap: 26px;
+      align-items: center;
+    }
+    .title {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+      margin-bottom: 25px;
+    }
+    h1 {
+      font-size: 42px;
+      font-weight: 100;
+      margin: 0;
     }
   `;
   return (
     <div class="game-title">
       <div class="picto">
-        {gameModes[props.gameOptions.modeSelected].head()}
+        {gameModes[props.gameOptions.modeSelected].smile()}
       </div>
       <div class="title">
         <h1>{`${props.t("gameMode")[props.gameOptions.modeSelected].subtitle}`}</h1>
