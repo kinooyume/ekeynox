77c9460faafda44a6fdcde4f1ce7d75e810996fa
Martin Kinoo <martkinoo@gmail.com>
2024-08-22 14:08:23 +0200

    style: sync input style

--- src/components/ui/RadioGroup.tsx
+++ src/components/ui/RadioGroup.tsx
@@ -46,7 +46,7 @@ function RadioGroup<Value>(props: RadioGroupProps<Value>) {
       transition: all 100ms linear;
     }
     .radio-group:hover {
-      background-color: var(--color-surface-mixed-500);
+      background-color: var(--background-color);
     }
 
     .radio-group input:not(:checked) + label:hover {
