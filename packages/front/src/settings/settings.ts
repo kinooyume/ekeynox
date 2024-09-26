6faf14027f1169d0756db2bb7ecc742f8502931b
Martin Kinoo <martkinoo@gmail.com>
2024-09-26 10:22:49 +0200

    upd settings

--- packages/front/src/settings/settings.ts
+++ packages/front/src/settings/settings.ts
@@ -1,5 +1,43 @@
-import { KeyboardLayoutName, Locale, locales, localeToKeyboard, Settings, SettingsOriginType, Theme } from "common/settings"
+export const keyboardLayoutName = ["qwerty", "azerty"];
+export type KeyboardLayoutName = (typeof keyboardLayoutName)[number];
 
+export const locales = ["en", "fr"];
+export type Locale = (typeof locales)[number];
+
+export type LocaleToKeyboard = Record<Locale, KeyboardLayoutName>;
+
+export const localeToKeyboard: LocaleToKeyboard = {
+  en: "qwerty",
+  fr: "azerty",
+};
+
+export enum SettingsOriginType {
+  auto,
+  user,
+}
+
+export type SettingsOrigin<T> =
+  | {
+      kind: SettingsOriginType.auto;
+      value: T;
+    }
+  | {
+      kind: SettingsOriginType.user;
+      value: T;
+    };
+
+export enum Theme {
+  light,
+  dark,
+  auto,
+}
+
+export type Settings = {
+  theme: SettingsOrigin<Theme>;
+  locale: SettingsOrigin<Locale>;
+  kb: SettingsOrigin<KeyboardLayoutName>;
+  showKb: boolean;
+};
 const toLocale = (s: string): Locale | null => (locales.includes(s) ? s : null);
 
 const getDefaultLocale = (): Locale | null => {
