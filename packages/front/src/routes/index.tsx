41074861dcea4102101969434255f91333ded0ed
Martin Kinoo <martkinoo@gmail.com>
2024-10-02 09:56:32 +0200

    front: fix secondary color dark theme

--- packages/front/src/routes/index.tsx
+++ packages/front/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { usePreloadRoute } from "@solidjs/router";
 import { onMount } from "solid-js";
 import { Portal } from "solid-js/web";
 import About from "~/components/about/About";
@@ -21,11 +22,6 @@ export default function Menu() {
 
     navigation.start(pendingMode, persistedGameOptions);
   };
-
-  onMount(() => {
-    navigation.menu();
-
-  })
 
   return (
     <div>
